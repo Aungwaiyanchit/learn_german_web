9dcace41f000c0ccc94af3afe3d7ea1691c0db66
Aung Wai Yan Chit <awyc@mit.com.mm>
2025-10-26 20:48:24 +0630

    remove API caching implementation; revert to basic axios instance

--- src/app/lib/helpers/api.ts
+++ src/app/lib/helpers/api.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
-import { attachApiCache } from "./api-cache";
 
-export const api = attachApiCache(
-    axios.create({
-        baseURL: process.env.NEXT_PUBLIC_API_URL,
-        headers: {
-            "Content-Type": "application/json",
-        },
-    }),
-);
+export const api = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
